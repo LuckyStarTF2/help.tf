40651e928989f2e62304fe91bb635a39b25563c2
malinov_k <malinov@jivosite.com>
2021-10-29 19:21:04 +0300

    added some imports, solved all issues

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,17 @@
 from flask import Flask
+import os
+from flask_login import LoginManager
+from flask_openid import OpenID
+from config import basedir
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 helptf = Flask(__name__)
+helptf.config.from_object('config')
+db = SQLAlchemy(helptf)
+lm = LoginManager(helptf)
+lm.init_app(helptf)
+oid = OpenID(helptf, os.path.join(basedir, 'tmp'))
+migrate =Migrate(helptf, db)
 
 from app import routes
