5f00653b2b7cf131de9fec26605c26f4b0b5c10a
LuckyStar <luckystartf2@gmail.com>
2021-11-16 20:07:19 +0300

    Debugging heroku db

--- config_heroku.py
+++ config_heroku.py
@@ -7,6 +7,7 @@ class Config(object):
     STEAM_API_KEY = os.environ.get('STEAM_API_KEY')
     OPENID_PROVIDERS = [{'name': 'Steam', 'url': 'https://steamcommunity.com/openid'}]
     DATABASE_URL = os.environ.get('JAWSDB_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('JAWSDB_URL')
     SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_MENTORS_PER_REQUEST = 50
